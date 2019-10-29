0e3abf8041f72ddf766d836fc2835a283326191f
Andrey <nullnullru@gmail.com>
2019-10-29 14:10:09 +0300

    set time from 90 sec to 45

--- src/main/java/com/fungames/rockpapersheetapi/model/RoomModel.java
+++ src/main/java/com/fungames/rockpapersheetapi/model/RoomModel.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 
 @Getter
 public class RoomModel {
-    private static final long MAX_ACTIVITY_DELAY = 90 * 1000; // 90 sec
+    private static final long MAX_ACTIVITY_DELAY = 45 * 1000; // 45 sec
 
     private UUID id;
     private RoomUserModel user1;
