67dd9ed73673db31a77500d1d9596fabd36294c7
Andrey <nullnullru@gmail.com>
2019-10-29 12:25:03 +0300

    wip

--- src/main/java/com/fungames/rockpapersheetapi/route/RoomRoute.java
+++ src/main/java/com/fungames/rockpapersheetapi/route/RoomRoute.java
@@ -4,5 +4,5 @@ public class RoomRoute {
     public static final String ROOT = BaseRoute.ROOT + "room/";
 
     public static final String BY_ID = ROOT + "{id}/";
-    public static final String RESULT = BY_ID + "result";
+    public static final String RESULT = BY_ID + "result/";
 }
